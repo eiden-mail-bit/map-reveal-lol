ada028878c0fe46fa2d0a7aff16e14cc501676d0
eiden-mail-bit <eiden-mail@t-online.de>
2026-01-28 23:05:58 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string appName = "C++ Project Marker";
+    std::cout << "Running " << appName << std::endl;
+    std::cout << "GitHub should detect this repository as C++." << std::endl;
+    return 0;
+}
---
+++ utils.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void printStatus() {
+    std::cout << "Utility module loaded successfully." << std::endl;
+}
